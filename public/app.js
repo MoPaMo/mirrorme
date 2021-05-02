16df0fb5a0487799ac5424c7cbf8b209a41bfa31
MoPaMo <mpm1301@web.de>
2021-05-02 21:29:53 +0200

    add loadscreen

--- public/app.js
+++ public/app.js
@@ -1,7 +1,11 @@
-Vue.createApp({
+let vm=Vue.createApp({
   data() {
     return {
-      msgs: [{author:"Cicero", text:"Lorem Ipsum", date:"dies"}],
+      msgs: [{ author: "Cicero", text: "Lorem Ipsum", date: "dies" },
+      { author: "Cicero", text: "Lorem Ipsum", date: "dies" },
+      { author: "Cicero", text: "Lorem Ipsum", date: "dies" },
+      { author: "Cicero", text: "Lorem Ipsum", date: "dies" }],
+      loading:true
     };
   },
 }).mount("#main");
