992cc17db2568dbd96aec3d2fa9d9431f68c3c6e
MoPaMo <mpm1301@web.de>
2021-05-02 21:36:20 +0200

    add fake id, remove load screen when mounted

--- public/app.js
+++ public/app.js
@@ -1,11 +1,14 @@
 let vm=Vue.createApp({
   data() {
     return {
-      msgs: [{ author: "Cicero", text: "Lorem Ipsum", date: "dies" },
-      { author: "Cicero", text: "Lorem Ipsum", date: "dies" },
-      { author: "Cicero", text: "Lorem Ipsum", date: "dies" },
-      { author: "Cicero", text: "Lorem Ipsum", date: "dies" }],
+      msgs: [{ author: "Cicero", text: "Lorem Ipsum", date: "dies", id:'159985870458322944' },
+      { author: "Cicero", text: "Lorem Ipsum", date: "dies", id:'159985870458322944' },
+      { author: "Cicero", text: "Lorem Ipsum", date: "dies", id:'159985870458322944' },
+      { author: "Cicero", text: "Lorem Ipsum", date: "dies", id:'159985870458322944' }],
       loading:true
     };
   },
+  mounted(){
+      this.loading=false;
+  }
 }).mount("#main");
