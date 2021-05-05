574170bbfcdeae9ee49877ec01ef7303769445ca
MoPaMo <>
2021-05-05 08:56:31 +0000

    remove placeholder msgs

--- public/app.js
+++ public/app.js
@@ -2,38 +2,7 @@ let vm = Vue.createApp({
   data() {
     return {
       msgs: [
-        {
-          author: "Cicero",
-          text: "Lorem Ipsum",
-          date: "dies",
-          id: "159985870458322944",
-          img:
-            "https://cdn.discordapp.com/avatars/159985870458322944/b50adff099924dd5e6b72d13f77eb9d7.png?size=128",
-        },
-        {
-          author: "Cicero",
-          text: "Lorem Ipsum",
-          date: "dies",
-          id: "159985870458322944",
-          img:
-            "https://cdn.discordapp.com/avatars/159985870458322944/b50adff099924dd5e6b72d13f77eb9d7.png?size=128",
-        },
-        {
-          author: "Cicero",
-          text: "Lorem Ipsum",
-          date: "dies",
-          id: "159985870458322944",
-          img:
-            "https://cdn.discordapp.com/avatars/159985870458322944/b50adff099924dd5e6b72d13f77eb9d7.png?size=128",
-        },
-        {
-          author: "Cicero",
-          text: "Lorem Ipsum",
-          date: "dies",
-          id: "159985870458322944",
-          img:
-            "https://cdn.discordapp.com/avatars/159985870458322944/b50adff099924dd5e6b72d13f77eb9d7.png?size=128",
-        },
+        
       ],
       loading: true,
       load_reason: "Starting app…",
